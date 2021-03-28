c56827a354da1e8b2fd6c12edb7e10ec0f96c7e0
Ding Pang <dpang@middlebury.edu>
2021-03-28 20:53:06 +0800

    single style: progress Co-authored-by: Mark Wu rwu@middlebury.edu

--- single.py
+++ single.py
@@ -14,6 +14,7 @@ import numpy as np
 import time
 import functools
 import PIL.Image
+from dataclasses import dataclass
 
 
 def tensor_to_image(tensor):
@@ -57,10 +58,106 @@ def imshow(image, title=None):
 content_image = load_img(Content_Path)
 style_image = load_img(Style_Path)
 
-plt.subplot(1, 2, 1)
-imshow(content_image, 'Content Image')
-plt.subplot(1, 2, 2)
-imshow(style_image, 'Style Image')
-plt.show()
+# plt.subplot(1, 2, 1)
+# imshow(content_image, 'Content Image')
+# plt.subplot(1, 2, 2)
+# imshow(style_image, 'Style Image')
+# plt.show()
 
-x =
+x = tf.keras.applications.vgg19.preprocess_input(content_image*255)
+x = tf.image.resize(x, (224, 224))
+# Load Pre-trained VGG19
+# This would require a "good" certificate
+vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
+
+content_layers = ['block5_conv2']
+style_layers = ['block1_conv1',
+                'block2_conv1',
+                'block3_conv1',
+                'block4_conv1',
+                'block5_conv1']
+num_content_layers = len(content_layers)
+num_style_layers = len(style_layers)
+
+def vgg_layers(layer_names):
+  # Creates a vgg model that returns a list of intermediate output values.
+  # Load our model. Load pretrained VGG, trained on imagenet data
+  vgg.trainable = False # Gatys Modle doesn't train or let the model learn
+
+  outputs = [vgg.get_layer(name).output for name in layer_names]
+  # Defining a model using Keras API with specifying input/output
+  model = tf.keras.Model([vgg.input], outputs)
+  return model
+
+style_extractor = vgg_layers(style_layers)
+style_outputs = style_extractor(style_image*255)
+
+
+#Look at the statistics of each layer's output
+for name, output in zip(style_layers, style_outputs):
+  print(name)
+  print("  shape: ", output.numpy().shape)
+  print("  min: ", output.numpy().min())
+  print("  max: ", output.numpy().max())
+  print("  mean: ", output.numpy().mean())
+  print()
+
+def gram_matrix(input_tensor):
+  result = tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor)
+  input_shape = tf.shape(input_tensor)
+  num_locations = tf.cast(input_shape[1]*input_shape[2], tf.float32)
+  return result/(num_locations)
+
+class StyleContentModel(tf.keras.models.Model):
+  def __init__(self, style_layers, content_layers):
+    super(StyleContentModel, self).__init__()
+    self.vgg = vgg_layers(style_layers + content_layers)
+    self.style_layers = style_layers
+    self.content_layers = content_layers
+    self.num_style_layers = len(style_layers)
+    self.vgg.trainable = False
+
+#Function returns the style of style layers(in gram matrix) and content of content layer
+  def call(self, inputs):
+    "Expects float input in [0,1]"
+    inputs = inputs*255.0
+    preprocessed_input = tf.keras.applications.vgg19.preprocess_input(inputs)
+    outputs = self.vgg(preprocessed_input)
+    style_outputs, content_outputs = (outputs[:self.num_style_layers],
+                                      outputs[self.num_style_layers:])
+
+    style_outputs = [gram_matrix(style_output)
+                     for style_output in style_outputs]
+
+    content_dict = {content_name: value
+                    for content_name, value
+                    in zip(self.content_layers, content_outputs)}
+
+    style_dict = {style_name: value
+                  for style_name, value
+                  in zip(self.style_layers, style_outputs)}
+
+    return {'content': content_dict, 'style': style_dict}
+
+
+
+extractor = StyleContentModel(style_layers, content_layers)
+
+results = extractor(tf.constant(content_image))
+
+print('Styles:')
+for name, output in sorted(results['style'].items()):
+  print("  ", name)
+  print("    shape: ", output.numpy().shape)
+  print("    min: ", output.numpy().min())
+  print("    max: ", output.numpy().max())
+  print("    mean: ", output.numpy().mean())
+  print()
+
+print("Contents:")
+for name, output in sorted(results['content'].items()):
+  print("  ", name)
+  print("    shape: ", output.numpy().shape)
+  print("    min: ", output.numpy().min())
+  print("    max: ", output.numpy().max())
+  print("    mean: ", output.numpy().mean())
