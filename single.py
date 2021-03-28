855c139ef8352c1a5f8b321acc4ada0140f13efa
Ding Pang <dpang@middlebury.edu>
2021-03-28 21:46:07 +0800

    single: working without variation loss

--- single.py
+++ single.py
@@ -48,7 +48,7 @@ def load_img(path_to_img):
   return img
 
 #Function to display image
-def (image, title=None):
+def imshow(image, title=None):
   if len(image.shape) > 3:
     image = tf.squeeze(image, axis=0)
 
