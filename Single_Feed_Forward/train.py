d8ca9c3c268324892534edb31e8d4e933d2c6c18
Mark Wu <rwu@middlebury.edu>
2021-04-10 20:25:02 +0800

    started single feed forward Co-author: Ding Pang dpang@middlebury.edu

---
+++ Single_Feed_Forward/train.py
@@ -0,0 +1,13 @@
+"""
+Author: Ding Pang dpang@middlebury.edu
+        Mark Wu rwu@middlebury.edu  
+
+Function takes in an input, which is the path of the image whose style will be learnt
+"""
+
+import os
+import tensorflow as tf
+import argparse
+
+
+
---
+++ Single_Feed_Forward/trainer.py
@@ -0,0 +1,8 @@
+"""
+Author: Mark Wu rwu@middlebury.edu
+        Ding Pang dpang@middlebury.edu
+Class for training generative class
+"""
+
+import os
+import time
